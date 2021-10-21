e4f03e5a37642f253a0a01044aa0fdb7cd5a3990
MarinaQA2016 <marinaqatest2016@gmail.com>
2021-10-21 15:11:03 +0300

    HW(18.10), SEL-06,07,08 were done. All tests were added to TrelloTests class

--- src/com/company/tests/TrelloTests.java
+++ src/com/company/tests/TrelloTests.java
@@ -24,9 +24,60 @@ public class TrelloTests {
     }
 
     @Test
+    public void loginNegativeNoEmail() throws InterruptedException {
+        // --- Define login button and click ------
+        WebElement loginIcon = driver.findElement(By.xpath("//a[@href='/login']"));
+        loginIcon.click();
+        Thread.sleep(5000);
+        // --- Fill in email field ------
+        WebElement emailField = driver.findElement(By.id("user"));
+        emailField.click();
+        emailField.sendKeys("");
+        Thread.sleep(2000);
+        // ---- Fill in password field ----
+        WebElement passwordField = driver.findElement(By.id("password"));
+        passwordField.click();
+        passwordField.sendKeys("12345678");
+        Thread.sleep(3000);
+        //--- Press 'Log in' button ----
+        driver.findElement(By.id("login")).click();
+        Thread.sleep(3000);
+        // ----Print error message ----------
+        WebElement errorMessage = driver.findElement(By.cssSelector("#error>p"));
+        System.out.println("Error message text: " + errorMessage.getText());
+    }
+    @Test
+    public void loginNegativeLoginNotEmail() throws InterruptedException {
+        // --- Define login button and click ------
+        WebElement loginIcon = driver.findElement(By.xpath("//a[@href='/login']"));
+        loginIcon.click();
+        Thread.sleep(5000);
+        // --- Fill in email field ------
+        WebElement emailField = driver.findElement(By.id("user"));
+        emailField.click();
+        emailField.sendKeys("gjgywggjwhdg");
+        Thread.sleep(2000);
+        // ---- Fill in password field ----
+        WebElement passwordField = driver.findElement(By.id("password"));
+        passwordField.click();
+        passwordField.sendKeys("12345678");
+        Thread.sleep(3000);
+        //--- Press 'Log in' button ----
+        driver.findElement(By.id("login")).click();
+        Thread.sleep(3000);
+        // ----Print error message ----------
+        WebElement errorMessage = driver.findElement(By.cssSelector("#error>p"));
+        System.out.println("Error message text: " + errorMessage.getText());
+    }
+
+    @Test
     public void loginNegativePasswordIncorrect() throws InterruptedException {
         // --- Define login button and click ------
-        WebElement loginIcon = driver.findElement(By.cssSelector(".btn-link"));
+        // ------ old locator, not actual now -----
+        // WebElement loginIcon = driver.findElement(By.cssSelector(".btn-link"));
+        // ----- new locators, actual now------
+        //WebElement loginIcon = driver.findElement(By.xpath("//span[contains(text(),'Log in')]"));
+        WebElement loginIcon = driver.findElement(By.xpath("//a[@href='/login']"));
         loginIcon.click();
         Thread.sleep(5000);
         //----- Enter email -----
@@ -50,6 +101,35 @@ public class TrelloTests {
         // --- Print error message -------
         WebElement errorMessage = driver.findElement(By.id("login-error"));
         System.out.println("Error message: " + errorMessage.getText());
+
+    }
+    @Test
+    public void loginPositive() throws InterruptedException {
+        // --- Define login button and click ------
+        WebElement loginIcon = driver.findElement(By.xpath("//a[@href='/login']"));
+        loginIcon.click();
+        Thread.sleep(5000);
+        //----- Enter email -----
+        WebElement emailField = driver.findElement(By.id("user"));
+        emailField.click();
+        emailField.sendKeys("marinaqatest2019@gmail.com");
+        Thread.sleep(2000);
+        // ------ Press 'Login with Atlassian' button
+        WebElement loginAtlButton = driver.findElement(By.id("login"));
+        loginAtlButton.click();
+        Thread.sleep(3000);
+        //------ Enter Password -----
+        WebElement passwordField = driver.findElement(By.id("password"));
+        passwordField.click();
+        passwordField.sendKeys("marinaqa");
+        Thread.sleep(2000);
+        //--- Submit 'Log In' button -----
+        WebElement submitButton = driver.findElement(By.id("login-submit"));
+        submitButton.click();
+        Thread.sleep(20000);
+        // ---- Print name of the 'Boards' button ----
+        WebElement boardsButton = driver.findElement(By.xpath("//span[contains(text(),'Boards')]"));
+        System.out.println("Name of the 'Boards' button is " + boardsButton.getText());
 
     }
 
